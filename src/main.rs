58a61667a8ee84ef8ff0b1b558ca5c04f9c10410
Ivy <ivyjs@hendrix.edu>
2020-07-21 11:58:23 -0500

    Made logging verbosity dependent on command line flag

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,24 @@
 use std::io;
 use satellite_numeric::pddl_parser::make_satellite_problem_from;
 use anyhop::{process_expr_cmd_line, CmdArgs};
+
 fn main() -> io::Result<()> {
-    let logger = setup_logger();
     let cmdArgs = CmdArgs::new()?;
+    let verbosity:u32 = match cmdArgs.num_from("v") {
+        Some(n) => n,
+        None => 0,
+    };
+    let logger = setup_logger(verbosity);
     return process_expr_cmd_line(&make_satellite_problem_from, &cmdArgs);
 }
 
-pub fn setup_logger() -> Result<(), fern::InitError> {
+pub fn setup_logger(verbosity:u32) -> Result<(), fern::InitError> {
+    let vebosity_as_object = match verbosity{
+        1=> log::LevelFilter::Warn,
+        2=> log::LevelFilter::Info,
+        3=> log::LevelFilter::Debug,
+        _=> log::LevelFilter::Info, //By default, info is a good level to have anything. Any less, and you don't get to see the results.
+    };
     fern::Dispatch::new()
         .format(|out, message, record| {
             out.finish(format_args!(
@@ -18,7 +29,7 @@ pub fn setup_logger() -> Result<(), fern::InitError> {
                 message
             ))
         })
-        .level(log::LevelFilter::Debug)
+        .level(vebosity_as_object)
         .chain(std::io::stdout())
         .chain(fern::log_file("output.log")?)
         .apply()?;
