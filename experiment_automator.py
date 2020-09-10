3c3e6ade854ef893e418f10aade0cc10fb3481dc
Ivy <ivyjs@hendrix.edu>
2020-09-09 20:56:09 -0500

    Added multithreading to the experiment runner
    In order to run experiments faster, I have added the ability to run multiple experiments at once. Preferably, you must be running on a computer with 4+ cores in order to take full advantage of this feature. If you have less cores, you can manually adjust the number of workers. 
    
    This cuts the run time of the experiments down from over 12 minutes to around 4 minutes.

--- experiment_automator.py
+++ experiment_automator.py
@@ -1,6 +1,19 @@
 import os
-for i in range(1,21):
-    experiment_name="pfile"+ str(i)
-    command="cargo run -- -15s -2v ../Numeric/"+experiment_name
-    print(command)
+import threading
+import subprocess
+import sys
+
+def runCommand(command):
     os.popen(command).read()
+
+def worker(min, max):
+    for i in range(min,max):
+        experiment_name="pfile"+ str(i)
+        command="cargo run -- -15s -2v ../Numeric/"+experiment_name
+        print(command)
+        runCommand(command)
+
+worker1 = threading.Thread(target=worker, args=([1,5])).start()
+worker2 = threading.Thread(target=worker, args=([6,10])).start()
+worker3 = threading.Thread(target=worker, args=([11,15])).start()
+worker4 = threading.Thread(target=worker,args=([16,21])).start()
